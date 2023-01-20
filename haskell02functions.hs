f774e42af447ff6aac08cefecf81489312cd92e6
Miguel-Jorge-SDV <69170859+Miguel-Jorge-SDV@users.noreply.github.com>
2023-01-20 15:37:30 -0300

    Add files via upload

--- haskell02functions.hs
+++ haskell02functions.hs
@@ -2,7 +2,7 @@ compTemp :: Float -> Bool
 compTemp num = num > 37.8
 
 hasFever :: [Float] -> [Float]
-hasFever list = filter compTemp list
+hasFever = filter compTemp
 
 insertH1 :: String -> String
 insertH1 word = "<li> " ++ word ++ " </li>"
@@ -10,4 +10,28 @@ insertH1 word = "<li> " ++ word ++ " </li>"
 itemize :: [String] -> [String]
 itemize list = map insertH1 list
 
-bigCircles :: Float -> [Float] -> [Float]    --Não consegui fazer esta questão
+circulo :: Float -> Float -> Bool
+circulo raio x = (2*3.14*raio) > x
+
+bigCircles :: Float -> [Float] -> [Float]
+bigCircles x list = filter (\raio -> (2*3.14*raio) > x) list
+
+temperatura :: Float -> Bool
+temperatura temp = temp > 37.8
+
+quarentine :: [(String,Float)] -> [(String,Float)]
+quarentine list = filter (\(_,temp) -> temp > 37.8) list
+
+idades :: Int -> Int -> Int
+idades birthAges ageRef = ageRef - birthAges
+
+agesIn :: [Int] -> Int -> [Int]
+agesIn birthAges ageRef = map (\birthAges -> ageRef - birthAges) birthAges
+
+sNames :: String -> Bool            --não sei como fazer esta questão e a seguinte tambem
+sNames nameList = nameList == "A"
+
+--"SUPER" ++ nameList
+
+superNames :: [String] -> [String]
+superNames nameList = map sNames
